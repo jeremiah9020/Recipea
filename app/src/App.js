ff041fd713f430d7fc1e233a7a734e528682d929
Jeremiah Lynn <jeremiahlynn@siliconpublishing.com>
2023-02-13 11:03:52 -0800

    Added some assets, organized some stuff

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
+import Header from './components/Header/Header'
 import './App.css';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Recipea</title>
+        <meta name="description" content="Recipea Web Application" />
+      </Helmet>
+      <Header></Header>
     </div>
   );
 }
---
+++ app/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import logo from '../../assets/logo.svg';
+import title from '../../assets/title.svg';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='Header-Bar'>
+            <img src={title} className="Header-Title" alt="logo" />
+            <img src={logo} className="Header-Icon" alt="logo" />
+            <p>Login, other stuff!</p>
+        </div>
+    )
+}
+
+export default Header;
