a6ea806991898100a08632b3d6d10bb9ab4d033a
Jeremiah Lynn <jeremiahlynn@siliconpublishing.com>
2023-02-13 19:40:24 -0800

    Added imports

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import Header from './components/Header/Header'
 import TestAPIConsumer from './components/TestAPIConsumer/TestAPIConsumer'
 
--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from '../../assets/logo.svg';
 import title from '../../assets/title.svg';
 import './Header.css';
