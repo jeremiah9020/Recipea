fe052b97360d422620d1e7b2a6046f9078448960
Logan Kloft <lkloft@outlook.com>
2023-04-01 16:25:55 -0700

    Add route for CreateRecipe page. Create basic Rich Textbox component. Todo: add toolbar for Rich Textbox.

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Authenticate/Login/Login";
 import Register from "./pages/Authenticate/Register/Register";
 import NoPage from "./pages/NoPage/NoPage";
+import CreateRecipe from "./pages/CreateRecipe/CreateRecipe";
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+        <Route path="create-recipe" element={<CreateRecipe />} />
         <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
