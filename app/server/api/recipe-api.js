b850de0bf879026bc0802f90bd067b60b0cdc659
Logan Kloft <lkloft@outlook.com>
2023-04-04 15:32:21 -0700

    Add get route to recipe-api that returns all recipes stored in the database. Add post route to recipe-api that adds a recipe to the recipe database.

--- app/server/api/recipe-api.js
+++ app/server/api/recipe-api.js
@@ -2,8 +2,31 @@ var express = require('express');
 var router = express.Router();
 const Recipe = require('../models/Recipe');
 
-router.get('/', (req, res, next) => {
-    res.send('in recipe api');
-})
+router.get('/', async (req, res, next) => {
+    try {
+        const recipes = await Recipe.findAll();
+        res.json(recipes);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try {
+        const recipe = await Recipe.create({
+            user: req.body.user,
+            title: req.body.title,
+            image: req.body.image,
+            time: req.body.time,
+            ingredients: req.body.ingredients,
+            tags: req.body.tags,
+            steps: req.body.steps,
+            description: req.body.description
+        });
+        res.json(recipe);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
 
 module.exports = router;
