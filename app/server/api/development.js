bfb259cdcbe4f5cbbc72886a020e515866533533
Jeremiah Lynn <jeremiahlynn@siliconpublishing.com>
2023-04-05 15:43:15 -0700

    Added development routes

---
+++ app/server/api/development.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const Recipe = require('../models/Recipe')
+const User = require('../models/User')
+
+router.delete('/recipes/:id', async (req, res, next) => {
+    const id = req.params.id
+    try {
+        const recipe = await Recipe.findByPk(id)
+        recipe.destroy()
+        res.send(`Deleted recipe with id ${id}`)
+    } catch (err) {
+        res.send(`Recipe with id ${id} not found`)
+    }
+})
+
+router.delete('/recipes', async (req, res, next) => {
+    Recipe.truncate()
+    res.send('Deleted all recipes')
+})
+
+router.delete('/users/:id', async (req, res, next) => {
+    const id = req.params.id
+    try {
+        const user = await User.findByPk(id)
+        user.destroy()
+        res.send(`Deleted user with id ${id}`)
+    } catch (err) {
+        res.send(`User with id ${id} not found`)
+    }
+})
+
+router.delete('/users', async (req, res, next) => {
+    User.truncate()
+    res.send('Deleted all users')
+})
+
+module.exports = router
