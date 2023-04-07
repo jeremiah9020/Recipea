a4f2488a9c85783a9a0ff40f9f465500a9b94af0
Logan Kloft <lkloft@outlook.com>
2023-04-07 14:53:23 -0700

    Add IngrientItem

---
+++ app/src/pages/CreateRecipe/IngredientItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function IngredientItem() {
+  return (
+    <div className="IngredientItem">
+        <input type="text" placeholder='enter ingredient'></input>
+    </div>
+  )
+}
+
+export default IngredientItem
