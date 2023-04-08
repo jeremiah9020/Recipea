7d5534c61af1eef346c938e6d741daf7ec483d44
Logan Kloft <lkloft@outlook.com>
2023-04-08 13:19:51 -0700

    Create Card component.

--- app/server/api/profiles.js
+++ app/server/api/profiles.js
@@ -3,6 +3,15 @@ const router = express.Router()
 const authMiddleware = require('../middleware/auth')
 const UserProfile = require('../models/UserProfile')
 
+router.get('/', async (req, res, next) => {
+    try {
+        const profiles = await UserProfile.findAll();
+        res.json(profiles);   
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
 router.get('/:id', async (req, res, next) => {
     const id = req.params.id;
     try {
--- app/src/pages/Home/Home.js
+++ app/src/pages/Home/Home.js
@@ -1,8 +1,20 @@
 import Helmet from 'react-helmet'
 import Header from '../../components/Header/Header'
 import TestAPIConsumer from '../../components/TestAPIConsumer/TestAPIConsumer'
+import Card from '../../components/Card/Card'
 
 function Home() {
+  const recipe = {
+    'title': 'Yummy Flapjacks',
+    'time': '00:25:00',
+    'tags':'glutenfree:breakfast',
+    'ingredients':'2 cup Flour:1 tbsp Baking Powder:1 cup Sugar:2 tsp Salt:2 Eggs:1/2 cup Milk:1/2 cup Butter',
+    'image':null,
+  }
+  const user = {
+    'username': 'MasterChef',
+    'profilepicture': null,
+  }
   return (
     <div className="App">
       <Helmet>
@@ -11,6 +23,7 @@ function Home() {
         <meta name="description" content="Recipea Web Application" />
       </Helmet>
       <Header></Header>
+      <Card recipe={recipe} user={user} />
       <TestAPIConsumer></TestAPIConsumer>
     </div>
   )
