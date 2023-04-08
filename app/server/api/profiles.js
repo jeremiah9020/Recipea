8844a192659642fbf610f8ac021171810c4ece26
Jeremiah Lynn <jeremiahlynn@siliconpublishing.com>
2023-04-08 14:49:27 -0700

    Added authenticated route for profile

--- app/server/api/profiles.js
+++ app/server/api/profiles.js
@@ -12,6 +12,20 @@ router.get('/', async (req, res, next) => {
     }
 })
 
+router.get('/authenticated', authMiddleware, async (req, res, next) => {
+    try {
+        const profile = await UserProfile.findByPk(req.user.id);
+        if (profile !== null)
+        {
+            res.json(profile);
+        } else {
+            res.json(`failed to find user with id ${req.user.id}`);
+        }
+    } catch(error) {
+        res.status(500).json(error);
+    }
+})
+
 router.get('/:id', async (req, res, next) => {
     const id = req.params.id;
     try {
@@ -26,5 +40,7 @@ router.get('/:id', async (req, res, next) => {
         res.status(500).json(error);
     }
 })
+
+
 
 module.exports = router;
