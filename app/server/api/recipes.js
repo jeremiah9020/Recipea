80efb523320ee8bcf43178c5a3969108b6a7092e
Jeremiah Lynn <jeremiahlynn@siliconpublishing.com>
2023-04-09 12:46:54 -0700

    Added image uploading

--- app/server/api/recipes.js
+++ app/server/api/recipes.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../middleware/auth')
 const Recipe = require('../models/Recipe')
+const fs = require('fs')
+const path = require('path')
+const uuid = require('uuid')
+const multer = require('multer')
+const upload = multer()
+const imagesPath = path.resolve(process.cwd(), 'server/images');
 
 router.get('/', async (req, res, next) => {
     try {
@@ -12,19 +18,38 @@ router.get('/', async (req, res, next) => {
     }
 });
 
-router.post('/', authMiddleware, async (req, res, next) => {
+router.post('/', authMiddleware, upload.single('image'), async (req, res, next) => { 
+    let image = null
+    if (req.file) {
+        const name = req.file.originalname
+        const nameArray = name.split('.')
+        const extension = nameArray[nameArray.length - 1]
+        const uid = uuid.v4()
+        const buffer = req.file.buffer
+    
+        const filepath = `${uid}.${extension}`
+
+        try {
+            fs.writeFileSync(`${imagesPath}/${filepath}`, buffer)
+        } catch (e) {
+            res.status(500).json(e);
+        }
+
+        image = filepath
+    }
+
     try {
         const recipe = await Recipe.create({
             userid: req.user.id,
             title: req.body.title,
-            image: req.body.image,
+            image: image,
             time: req.body.time,
             ingredients: req.body.ingredients,
             tags: req.body.tags,
             steps: req.body.steps,
             description: req.body.description
         });
-        res.json(recipe);
+        res.status(200).json(recipe);
     } catch (error) {
         res.status(500).json(error);
     }
