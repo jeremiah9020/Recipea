f0863b94903dd48fbb2776063c347d9c965982df
Jeremiah Lynn <jeremiahlynn@siliconpublishing.com>
2023-04-09 17:38:36 -0700

    removed unused line

--- app/src/pages/CreateRecipe/CreateRecipe.jsx
+++ app/src/pages/CreateRecipe/CreateRecipe.jsx
@@ -1,6 +1,5 @@
 import {React, useEffect, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
-import 'draft-js/dist/Draft.css';
 
 // stylesheets
 import './CreateRecipe.scss';
