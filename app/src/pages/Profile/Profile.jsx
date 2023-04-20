651f8860a0881744f80a22efdf3f97ae0291c971
Jeremiah Lynn <jeremiahlynn@siliconpublishing.com>
2023-04-20 12:36:15 -0700

    Added profile updating

--- app/src/pages/Profile/Profile.jsx
+++ app/src/pages/Profile/Profile.jsx
@@ -13,6 +13,7 @@ function Profile() {
     const [editable, setEditable] = useState(false)
     const [username, setUsername] = useState('')
     const [description, setDescription] = useState('')
+    const [descriptionChanged, setDescriptionChanged] = useState(false)
     const [profilePicture,setProfilePicture] = useState(<DefaultProfilePicture className="Default"/>)
     const [profileBanner,setProfileBanner] = useState(<div style={{"background-color": "rgb(200,200,200)","width":"100%","height":"100%"}}/>)
 
@@ -52,6 +53,8 @@ function Profile() {
                 navigate('/')
             } else {
                 const profile = await response.json()
+
+                console.log(profile)
                 if (profile.username === authenticatedUsername) setEditable(true)
                 setUsername(profile.username)
 
@@ -64,6 +67,70 @@ function Profile() {
         getData()
     },[searchParams,setSearchParams,navigate])
 
+    function textAreaChangeHandler({target: {value}}) {
+        setDescriptionChanged(description !== value)
+    }
+
+    function textAreaBeforeInputHandler(e) {
+        const value = e.target.value + e.data
+        const byLine = value.split('\n')
+
+        let length = 0
+        for (let line of byLine) {
+            length += Math.max(line.length,40)
+        }
+
+        if (length > 200) e.preventDefault()
+    }
+
+    async function updateProfile(formData) {
+        const url = 'http://localhost:3001/api/profiles/updateinfo'
+        return await fetch(url, {
+            method: 'PATCH',
+            credentials: 'include',
+            body: formData
+        });
+    }
+
+    async function submitDescriptionChange() {
+        const formData = new FormData()
+        const description = document.getElementById("ProfileDescription").value
+        formData.append("description",description)
+
+        await updateProfile(formData)
+
+        setDescription(description)
+        setDescriptionChanged(false)
+    }
+
+    async function changeProfilePicture({target}) {
+        const formData = new FormData()
+        const profilepicture = target.files[0]
+
+        //Resets the files
+        target.files = new DataTransfer().files
+
+        formData.append("profilepicture",profilepicture)
+
+        const response = await updateProfile(formData)
+        const profile = await response.json()
+         setProfilePicture(<img src={'http://localhost:3001/static/' + profile.profilepicture} alt="Profile"/>)
+    }
+
+    async function changeProfileBanner({target}) {
+        const formData = new FormData()
+        const profilebanner = target.files[0]
+
+        //Resets the files
+        target.files = new DataTransfer().files
+
+        formData.append("profilebanner",profilebanner)
+
+        const response = await updateProfile(formData)
+        const profile = await response.json()
+        setProfileBanner(<img src={'http://localhost:3001/static/' + profile.profilebanner} alt="Profile"/>)
+    }
+
     return (
         <div className="Profile">
             <Helmet>
@@ -74,21 +141,38 @@ function Profile() {
             <Header/>
 
             <div className="BannerContainer">
+                {profileBanner}
+                {editable &&
+                <label className="BannerInput">
+                    <input type="file" accept="image/jpeg, image/png, image/jpg" onInput={changeProfileBanner}/>
+                </label>}
                 <div className="Nametag">
                     <div class="ProfilePicture">
                         {profilePicture}
+                        {editable &&
+                        <label className="ImageInput">
+                            <input type="file" accept="image/jpeg, image/png, image/jpg" onInput={changeProfilePicture}/>
+                        </label>}
                     </div>
                     <div className="NameBox">
                         <div className="Chef">Chef</div>
                         <div className="Username">@{username}</div>
                     </div>
                 </div>
-                {profileBanner}
             </div>
-            <div>
-                
+            <div class="DescriptionContainer">
+               {description && <textarea 
+                    defaultValue={description} 
+                    class="DescriptionArea" 
+                    id="ProfileDescription" 
+                    readOnly={!editable} 
+                    onBeforeInput={textAreaBeforeInputHandler} 
+                    onChange={textAreaChangeHandler}/>
+               }
+
+                {editable && descriptionChanged && 
+                <input className="SaveDescriptionChange" type="button" value="SAVE" onClick={submitDescriptionChange}/>}
             </div>
-            {editable && <p>EDITABLE</p>}
         </div>
   )
 }
